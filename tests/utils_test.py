dad3f814003707c38a07ecf4e7e25f9e2d179212
Aasif Faizal <38973423+aasiffaizal@users.noreply.github.com>
2022-01-19 21:47:38 +0530

    Fix: Headers order in csv (#7)

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -6,16 +6,26 @@ from sqlmodel import SQLModel
 from utils import get_csv_from_orm
 
 
-def test_get_csv_from_orm_returns_string():
-    class MockModel(SQLModel):
-        head1: str
-        head2: str
+class MockModel(SQLModel):
+    head1: str
+    head2: str
 
-    mock_item = MockModel(**{
-        'head1': '1',
-        'head2': '2'
-    })
+
+mock_item = MockModel(**{
+    'head1': '1',
+    'head2': '2'
+})
+
+
+def test_get_csv_from_orm_returns_string():
     string = get_csv_from_orm([mock_item])
+    reader = csv.DictReader(string)
+    rows = [row for row in reader]
+    assert jsonable_encoder(mock_item) == rows.pop()
+
+
+def test_get_csv_from_orm_with_headers_returns_string():
+    string = get_csv_from_orm([mock_item], headers=mock_item.dict().keys())
     reader = csv.DictReader(string)
     rows = [row for row in reader]
     assert jsonable_encoder(mock_item) == rows.pop()
